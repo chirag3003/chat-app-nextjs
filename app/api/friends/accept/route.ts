f13085a473b78b52c629b806fef99b180e87c6be
Chirag Bhalotia <chiragbhalotia0412@gmail.com>
2023-06-27 08:17:57 +0530

    updaetd accept friend request route

--- app/api/friends/accept/route.ts
+++ app/api/friends/accept/route.ts
@@ -5,30 +5,30 @@ import {NextResponse} from "next/server";
 import {fetchRedis} from "@/helpers/redis";
 import {db} from "@/lib/db";
 
-export async function POST(req:Request){
-    try{
-        const body = await  req.json();
-        const {id:idToAdd} = z.object({id:z.string()}).parse(body)
+export async function POST(req: Request) {
+    try {
+        const body = await req.json();
+        const {id: idToAdd} = z.object({id: z.string()}).parse(body)
         const session = await getServerSession(authOptions);
-        if(!session){
-            return NextResponse.json("Not Authorised",{status:401})
+        if (!session) {
+            return NextResponse.json("Not Authorised", {status: 401})
         }
-        const isAlreadyFriends = (await fetchRedis("sismember",`user:${session.user.id}:friends`,idToAdd)) as 0|1
-        if(isAlreadyFriends){
-            return NextResponse.json("Already Friends",{status:400})
+        const isAlreadyFriends = (await fetchRedis("sismember", `user:${session.user.id}:friends`, idToAdd)) as 0 | 1
+        if (isAlreadyFriends) {
+            return NextResponse.json("Already Friends", {status: 400})
         }
-        const hasFriendRequest = (await fetchRedis("sismember",`user:${session.user.id}:incoming_friend_requests`,idToAdd)) as 0|1
-        if(!hasFriendRequest){
-            return NextResponse.json("No Friends Request",{status:400})
+        const hasFriendRequest = (await fetchRedis("sismember", `user:${session.user.id}:incoming_friend_requests`, idToAdd)) as 0 | 1
+        if (!hasFriendRequest) {
+            return NextResponse.json("No Friends Request", {status: 400})
         }
-        await db.srem(`user:${session.user.id}:incoming_friend_requests`,idToAdd)
-        await db.sadd(`user:${session.user.id}:friends`,idToAdd)
-        await db.sadd(`user:${idToAdd}:friends`,session.user.id)
+        await Promise.all([db.srem(`user:${session.user.id}:incoming_friend_requests`, idToAdd),
+            db.sadd(`user:${session.user.id}:friends`, idToAdd),
+            db.sadd(`user:${idToAdd}:friends`, session.user.id)])
         return new NextResponse("ok")
     } catch (e) {
         console.log(e)
-        if(e instanceof z.ZodError){
-            return NextResponse.json("Unprocessable request payload",{status:422})
+        if (e instanceof z.ZodError) {
+            return NextResponse.json("Unprocessable request payload", {status: 422})
         }
     }
 }
